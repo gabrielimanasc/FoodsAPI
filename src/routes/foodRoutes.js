f15a83456a54be5ed9ee2d063fc221b6c023dad1
Gabriel Lima do Nascimento <96126497+gabrielimanasc@users.noreply.github.com>
2024-11-05 23:47:10 -0300

    Update foodRoutes.js

--- src/routes/foodRoutes.js
+++ src/routes/foodRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const foodController = require('../controllers/foodController');
 
-router.get('/', foodController.getAllFoods);
-router.post('/', foodController.createFood);
+router.get('/', foodController.getAllFoods);  // Rota para obter todos os alimentos
+router.post('/', foodController.createFood);  // Rota para criar um alimento novo
 router.get('/:id', foodController.getFoodById); // Rota para obter um alimento por ID
 router.put('/:id', foodController.updateFood);  // Rota para editar um alimento por ID
 router.delete('/:id', foodController.deleteFood); // Rota para excluir um alimento por ID
